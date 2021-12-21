81f7f03e03b2e9d0f8731584acfa925b507307c3
qlicpotter <cpotter@qualitylogic.com>
2021-12-21 15:01:34 -0800

    getting file locally

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,28 @@
 import Head from 'next/head'
+import { useState } from 'react'
+import { Row, Col, Divider, Typography, Layout ,Input } from 'antd'
 
-export default function Home() {
+const { Header, Footer, Content } = Layout
+const { Title } = Typography
+
+import 'antd/dist/antd.dark.css'
+
+export default function Home(props) {
+  const [coverageData, setCoverageData] = useState(null);
+
+  const downloadToClient = async (event) => {
+    if (event.target.files && event.target.files[0]) {
+      const jsonFile = event.target.files[0];
+      
+      let jsonText = await jsonFile.text()
+      let p = document.createElement('p')
+      p.innerHTML = jsonText
+      document.querySelector("main").appendChild(p)
+      console.log(JSON.parse(jsonText))
+      setCoverageData(JSON.parse(jsonText));
+    }
+  };
+
   return (
     <div>
       <Head>
@@ -8,14 +30,27 @@ export default function Home() {
         <meta name="description" content="Code coverage report dashboard" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Layout>
+        <Header>
+          <Title>Xcode Coverage Parser</Title>
+        </Header>
+        <Content>
+          <Row justify="center">
+            <Col span={12}>
+              <label htmlFor='coverageData'>Choose a coverage JSON file:</label>
+              <Input name='coverageData' type='file' onChange={downloadToClient} />
+            </Col>
+          </Row>
+            
+            
+          
+          <Divider></Divider>
+        </Content>
+        <Footer>
 
-      <main>
-        
-      </main>
-
-      <footer>
-        
-      </footer>
+        </Footer>
+      </Layout>
+      
     </div>
   )
 }
---
+++ scripts/parser.js
@@ -0,0 +1,7 @@
+import coverageData from './coverage.json'
+
+const log = () => {
+    console.log(coverageData)
+}
+
+export default log
