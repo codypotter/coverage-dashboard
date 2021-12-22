1815dd98b305e8a0228476ae0f6dd27d8ad2a029
Cody Potter <enjoipotter@gmail.com>
2021-12-21 18:07:30 -0800

    fixed header styling and spacing

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import Head from 'next/head'
 import { useState } from 'react'
-import { Row, Col, Divider, Typography, Layout ,Input } from 'antd'
+import { PageHeader, Space, Steps, Row, Col, Divider, Typography, Layout ,Input } from 'antd'
 
 const { Header, Footer, Content } = Layout
 const { Title } = Typography
+const { Step } = Steps
 
 import 'antd/dist/antd.dark.css'
 
@@ -30,21 +31,41 @@ export default function Home(props) {
         <meta name="description" content="Code coverage report dashboard" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Layout>
+      <Layout hasSider={false}>
         <Header>
-          <Title>Xcode Coverage Parser</Title>
+          <PageHeader
+            backIcon={false}
+            title="Xcode Coverage Dashboard"
+          />
+
         </Header>
         <Content>
+          <Row justify="center" style={{marginTop: "16px"}}>
+            <Col>
+              <Steps direction="vertical" size="small" current={-1}>
+                <Step title="Enable" description="Change your Xcode build scheme settings to gather code coverage">
+                  Here is some stuff inside?
+                </Step>
+                <Step title="Test" description="Kick off a test run and reveal in Finder">
+
+                </Step>
+                <Step title="Convert" description="xcrun xccov view --report --json <test>.xcresult">
+
+                </Step>
+                <Step title="Upload" description="Upload the json file to this page">
+
+                </Step>
+              </Steps>
+            </Col>
+          </Row>
           <Row justify="center">
-            <Col span={12}>
+            <Col>
               <label htmlFor='coverageData'>Choose a coverage JSON file:</label>
               <Input name='coverageData' type='file' onChange={downloadToClient} />
             </Col>
           </Row>
-            
-            
-          
-          <Divider></Divider>
+          <Divider/>
+
         </Content>
         <Footer>
 
